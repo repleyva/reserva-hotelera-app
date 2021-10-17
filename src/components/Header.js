e035ee68963eee7656059e9e154c80eb604b2130
repleyva <rusvelpassos@gmail.com>
2021-10-17 10:53:11 -0500

    Maqueta de Header Terminada

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,60 @@
-import { AppBar, InputBase, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  ButtonBase,
+  Drawer,
+  IconButton,
+  InputBase,
+  List,
+  ListItem,
+  Toolbar,
+} from "@mui/material";
 import React, { useState } from "react";
 import "./header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Link } from "react-router-dom";
+
+const menuList = ["My Account", "Previous bookings", "Logout"];
 
 const Header = () => {
-  const [mobile, setMobile] = useState(false);
-  const displayMobile = () => {};
-  const displayDesktop = () => {
-    return (
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+  const getDrawerChoices = () => {
+    return menuList.map((data) => {
+      return (
+        <ButtonBase className="menu-list-button">
+          <List className="menu-list">
+            <ListItem className="menu-item">{data}</ListItem>
+          </List>
+        </ButtonBase>
+      );
+    });
+  };
+  return (
+    <AppBar className="app-bar">
       <Toolbar className="tool-bar">
-        <img src="assets/logo.png" alt="logo" className="logo-header" />
+        <IconButton {...{ onClick: handleDrawerOpen }} className="menu-button">
+          <MenuIcon className="menu-icon" />
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: "left",
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div className="menu-drawer">{getDrawerChoices()}</div>
+        </Drawer>
+        <Link to="/" className="link-logo">
+          <img src="assets/logo.png" alt="logo" className="logo-header" />
+        </Link>
         <nav className="search-nav">
           <InputBase
             fullWidth
@@ -24,11 +68,6 @@ const Header = () => {
           <AccountCircleIcon className="profile-icon" />
         </div>
       </Toolbar>
-    );
-  };
-  return (
-    <AppBar className="app-bar">
-      {mobile ? displayMobile() : displayDesktop()}
     </AppBar>
   );
 };
