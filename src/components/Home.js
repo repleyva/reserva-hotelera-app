3bd026369e59b152fb3114ac719f7a6087ac093b
repleyva <rusvelpassos@gmail.com>
2021-10-17 11:02:47 -0500

    Agregando estilos al home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
 
 const Home = () => {
-	return (
-		<div>
-			<h1>Home</h1>
-		</div>
-	)
-}
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
